c1d6653a404453a468d152004ef1752e2a51f20c
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2022-09-26 22:03:59 +0300

    Add files via upload

---
+++ While Foreach/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ***WHILE***
+            // 1 den baslayarak console dan girilen sayiya kadar (sayi dahil) ortalama hesaplayip, yazdirin.
+
+            Console.WriteLine("Bir sayi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int toplam = 0, sayac = 1;
+            
+            while (sayi >= sayac)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("Ortalama: " + (toplam / sayi));
+
+
+            // 'a' dan 'z' ye kadar tum harfleri console a yazdirin.
+
+            char character = 'a';
+
+            while (character <= 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+
+            // ***FOREACH*** 
+
+            string[] arabalar = { "Mercedes", "Honda", "Volswagen", "Toyota" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
