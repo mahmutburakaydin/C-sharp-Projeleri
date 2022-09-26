7f399295597a6074433806412f911500d479ec8f
Mahmut Burak Aydin <109317317+mahmutburakaydin@users.noreply.github.com>
2022-09-26 22:15:02 +0300

    Add files via upload

---
+++ Array Sinifi Metodları/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ArraySinifiMetodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sort
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+            Console.WriteLine("**** Sirasiz Dizi ****");
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            Console.WriteLine("**** Sirali Dizi ****");
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            // Clear
+            Console.WriteLine("**** Array Clear ****");
+            // sayiDizisi elemanlarini kullanarak 2. indexten itibaren 2 tane elemani sifirlar.
+            Array.Clear(sayiDizisi, 2, 2);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            // Reverse
+            Console.WriteLine("**** Array Reverse ****");
+            Array.Reverse(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+            // IndexOf
+            Console.WriteLine("**** Array IndexOf ****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+
+            // Resize
+            Console.WriteLine("**** Array Resize ****");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+        }
+    }
+}
